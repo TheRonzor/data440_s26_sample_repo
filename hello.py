3b710cd5d671f6a440c26c46e5e554a7e08723f6
Ron <ronniecougar@gmail.com>
2026-01-28 13:49:01 -0500

    wrote main.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from data440-s26-sample-repo!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,11 @@
+from src.data import make_data, save_data
+from src.viz import make_figure
+
+def main() -> None:
+    data = make_data()
+    save_data(data, 'test2.npy')
+    make_figure(data, 'test_figure.svg')
+    return None
+
+if __name__ == "__main__":
+    main()
